f08b71881e8852e0d2e24052964430c0099297ad
charlie <linkin8288@gmail.com>
2023-05-02 13:12:59 +0200

    First Commit

---
+++ src/main/java/com/luv2code/springcodedemo/SpringcodedemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springcodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcodedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcodedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luv2code/springcodedemo/SpringcodedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springcodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcodedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
